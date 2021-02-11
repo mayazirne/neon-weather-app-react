f3a874278d787686b163a5763fd3f6675254d3cb
mayazirne <73857372+mayazirne@users.noreply.github.com>
2021-02-11 17:42:50 +0000

    Added GitHub and LinkedIn links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was build by {" "}
+        <a
+        href="https://www.linkedin.com/in/maya-zirne-788888100/"
+        target="_blank"
+        >
+        Maya Zirne
+        </a> and is {" "}
+        <a
+        href="https://github.com/mayazirne/neon-weather-app-react"
+        target="_blank"
+        >
+        open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
