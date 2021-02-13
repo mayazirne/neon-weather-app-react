962f83b3f2388334a717964bea068cbd1d79921c
mayazirne <73857372+mayazirne@users.noreply.github.com>
2021-02-13 19:21:37 +0000

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from "react";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <footer>
         This project was build by {" "}
@@ -20,6 +21,7 @@ export default function App() {
         open-sourced on GitHub
         </a>
       </footer>
+      </div>
     </div>
   );
 }
